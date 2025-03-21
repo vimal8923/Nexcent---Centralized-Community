5491fba5b1b37b975c2a64651e023e542656f238
vimal8923 <vimalshahi2003@gmail.com>
2025-03-21 14:01:11 +0530

    finalpush

---
+++ project-root/assets/js/script.js
@@ -0,0 +1,8 @@
+function toggleSidebar() {
+    let sidebar = document.getElementById("sidebar");
+    if (sidebar.style.right === "0px") {
+        sidebar.style.right = "-250px";
+    } else {
+        sidebar.style.right = "0px";
+    }
+}
